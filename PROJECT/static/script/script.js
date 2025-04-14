55abed6d5b76ee1503e88676dfc8b24308a25a36
Marchel <marchelmanullang1@gmail.com>
2025-04-14 15:43:07 +0800

    Fix Hamburger Menu , add more display flexible

--- PROJECT/static/script/script.js
+++ PROJECT/static/script/script.js
@@ -2,13 +2,11 @@ document.addEventListener("DOMContentLoaded", function () {
   const hamburger = document.getElementById("hamburger");
   const navbarNav = document.getElementById("navbarNav");
 
-  // Saat hamburger diklik, toggle visibility menu
   hamburger.addEventListener("click", function (event) {
-    event.stopPropagation(); // Mencegah event bubble ke document
+    event.stopPropagation();
     navbarNav.classList.toggle("show");
   });
 
-  // Saat klik di luar menu, tutup menu
   document.addEventListener("click", function (event) {
     const isClickInside =
       navbarNav.contains(event.target) || hamburger.contains(event.target);
