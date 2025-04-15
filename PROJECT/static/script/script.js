a4632143ca651a8a51410bf7a5996704a099e746
Marchel <marchelmanullang1@gmail.com>
2025-04-15 22:29:09 +0800

    adding feature light mode & dark mode

--- PROJECT/static/script/script.js
+++ PROJECT/static/script/script.js
@@ -1,18 +1,38 @@
 document.addEventListener("DOMContentLoaded", function () {
   const hamburger = document.getElementById("hamburger");
   const navbarNav = document.getElementById("navbarNav");
+  let lightMode = localStorage.getItem("light-mode");
+  const themeSwitch = document.getElementById("themeSwitch");
 
-  hamburger.addEventListener("click", function (event) {
-    event.stopPropagation();
+  hamburger.addEventListener("click", function () {
     navbarNav.classList.toggle("show");
   });
 
   document.addEventListener("click", function (event) {
-    const isClickInside =
-      navbarNav.contains(event.target) || hamburger.contains(event.target);
-
-    if (!isClickInside) {
+    if (
+      !navbarNav.contains(event.target) &&
+      !hamburger.contains(event.target)
+    ) {
       navbarNav.classList.remove("show");
     }
+  });
+
+  const enableLightMode = () => {
+    document.body.classList.add("light-mode");
+    localStorage.setItem("light-mode", "active");
+  };
+
+  const disableLightMode = () => {
+    document.body.classList.remove("light-mode");
+    localStorage.setItem("light-mode", "null");
+  };
+
+  if (lightMode === "active") {
+    enableLightMode();
+  }
+
+  themeSwitch.addEventListener("click", () => {
+    lightMode = localStorage.getItem("light-mode");
+    lightMode !== "active" ? enableLightMode() : disableLightMode();
   });
 });
